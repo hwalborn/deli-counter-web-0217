ddaafd41927bc2c563bb8ee294d58a168e0faad2
Holt Walborn <hwalborn@gmail.com>
2017-01-05 19:25:51 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+katz_deli = []
+
+def line(deli_line)
+  if deli_line.size == 0
+    puts "The line is currently empty."
+  else
+    deli_line.each_with_index {|name, index| deli_line[index] = "#{index + 1}. #{name}"}
+    deli_str = deli_line.join(" ")
+    puts "The line is currently: #{deli_str}"
+  end
+end
+
+def take_a_number(deli_line, name)
+  deli_line << name
+  puts "Welcome, #{name}. You are number #{deli_line.length} in line."
+end
+
+def now_serving(deli_line)
+  if deli_line.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{deli_line.shift}."
+  end
+end
